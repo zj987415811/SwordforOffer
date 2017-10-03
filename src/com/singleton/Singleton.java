636eb5574c3cb55d6871bd80a074c9c954db3976
rockyzj <987415811@qq.com>
2017-10-03 17:42:29 +0800

    rockyzj
    内部类实现单例模式

--- src/com/singleton/Singleton.java
+++ src/com/singleton/Singleton.java
@@ -1,5 +1,11 @@
 package com.singleton;
 
 public class Singleton {
-
-}
+     private static class SingletonHolder {
+     private static final Singleton INSTANCE = new Singleton();
+     }
+     private Singleton (){}
+     public static final Singleton getInstance() {
+         return SingletonHolder.INSTANCE;
+    }
+ }  
