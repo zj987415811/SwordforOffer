a7a18e21dff70c3b7b6ff6f398e29c375a1937db
rockyzj <987415811@qq.com>
2017-10-09 21:11:10 +0800

    rocky
    递归和循环实现1+2.。。+n

---
+++ src/com/circulateandrecursive/MainTest.java
@@ -0,0 +1,25 @@
+package com.circulateandrecursive;
+
+import com.main.Main;
+
+public class MainTest {
+	static public void circulate() {
+		int sum=0;
+		int i=0;
+		while(i<=100) {
+			sum+=i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+	
+	public static void main(String[] args) {
+		MainTest.circulate();
+		System.out.println(MainTest.recursive(100));
+	}
+	static int recursive(int n) {
+		// TODO Auto-generated method stub
+		
+		return n<=0?0:n+recursive(n-1);
+	}
+}
